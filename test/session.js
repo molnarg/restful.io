9d97de1c131d9a6892ef306b73f7d54ee939d5be
Gábor Molnár <gabor.molnar@sch.bme.hu>
2011-12-27 19:32:43 +0100

    Making session tests more readable.

--- test/session.js
+++ test/session.js
@@ -2,100 +2,137 @@ var http   = require('http'),
     events  = require('events'),
     should = require('should');
 
+var testPort = 6000;
+
 var Session = require('../lib/session.js');
+
+var Request = function(options) {
+  var request = http.request({
+    method : options.method,
+    host :   'localhost',
+    port :   testPort,
+    path :   options.path,
+    headers : { 'Content-Type' : options.content_type }
+  });
+
+  request.ready = false;
+  request.on('response', function(res) {
+    request.response = res;
+    res.body = '';
+    res.on('data', function(data) {
+      res.body += data.toString();
+    });
+    res.on('end', function() {
+      request.ready = true;
+      request.emit('ready');
+    });
+  });
+
+  request.onReady = function(callback) {
+    if (request.ready) {
+      callback();
+    } else {
+      request.on('ready', callback);
+    }
+  };
+
+  request.start = function() {
+    request.end(options.data);
+  }
+
+  return request;
+};
 
 describe('A Session', function(){
   var session = new Session(),
       server = http.createServer(session.middleware);
-  
-  server.listen(6000);
-  
-  session.request = function(method, event, type, payload) {
-    var request = http.request({
-      method : method,
-      host :   'localhost',
-      port :   6000,
-      path :   event,
-      headers : { 'Content-Type' : type }
-    });
-    
-    if (payload !== undefined) {
-      setTimeout(function(){
-        request.end(payload);
-      }, 0);
-    }
-    
-    return request;
-  };
-  
+
+  server.listen(testPort);
+
   describe('in response to an HTTP PUT request', function(){
     describe('with JSON-encoded object as payload (Content-Type is "application/json")', function() {
-      var request = function(object) {
-        session.request('PUT', '/x/y', 'application/json', JSON.stringify(object));
-      };
-      
-      it('responds with 200 OK status code');
-      
+      var testObject = {a:1, b:2},
+          testEvent = 'x/y';
+
+      var request = new Request({
+        method       : 'PUT',
+        path         : '/' + testEvent,
+        content_type : 'application/json',
+        data         : JSON.stringify(testObject)
+      });
+
       it('emits the decoded object as an event', function(done){
-        var testObject = {a:1, b:2};
-        request(testObject);
-        session.once('x/y', function(eventdata) {
+        request.start();
+        session.once(testEvent, function(eventdata) {
           eventdata.should.eql(testObject);
           done();
         });
       });
+
+      it('responds with 200 OK status code');
     });
 
     describe('with wrong JSON data as payload', function() {
-      var request = function() {
-        return session.request('PUT', '/x/y', 'application/json', '"Error{}');
-      };
-      
+      var testEvent = 'x/y';
+
+      var request = new Request({
+        method       : 'PUT',
+        path         : '/' + testEvent,
+        content_type : 'application/json',
+        data         :  '"Error{}'
+      });
+
       it('responds with 400 Bad Request status code', function(done){
-        request().on('response', function(res) {
-          res.statusCode.should.equal(400);
+        request.start()
+        request.onReady(function() {
+          request.response.statusCode.should.equal(400);
           done();
         });
       });
     });
 
     describe('with binary data as payload (Content-Type isn\'t "application/json")', function() {
-      var request = function(data) {
-        session.request('PUT', '/x/y', 'application/octet-stream', data);
-      };
-      
-      it('responds with 200 OK status code');
-      
+      var testEvent = 'a',
+          testData = 'test';
+
+      var request = new Request({
+        method       : 'PUT',
+        path         : '/' + testEvent,
+        content_type : 'application/octet-steam',
+        data         :  testData
+      });
+
       it('emits the HTTP stream object as an event', function(done){
-        var testString = 'test';
-        request(testString);
-        session.once('x/y', function(stream) {
+        request.start();
+        session.once(testEvent, function(stream) {
           stream.should.be.an.instanceof(events.EventEmitter);
           stream.should.have.property('readable');
           stream.should.have.property('pipe');
           stream.pipe.should.be.a('function');
           stream.on('data', function(data) {
-            data.toString().should.equal(testString);
+            data.toString().should.equal(testData);
             done();
           });
         });
       });
+
+      it('responds with 200 OK status code');
     });
   });
 
   describe('in response to an HTTP GET request', function() {
     describe('which indicates client side SSE support (Accept header is "text/event-stream")', function() {
       it('responds with 200 OK status code');
-      
+
       it('streams matching events in \'data: {"type":"type", "event":"data"}\\n\\n\' format');
-      
+
       it('never closes the connection, waits for the client to do so');
     });
     describe('which doesnt indicate client side SSE support', function() {
       it('responds with 200 OK status code');
-      
+
       it('sends the name of the next matching event in the "Event" header field, and the data as the payload');
-      
+
       it('closes the connection after the first event');
     });
   });
